c13e224fa15b8a93bff4d40f4d6858e35fae11bf
shirazJafri <shirazJafri@outlook.com>
2022-04-13 01:17:50 +0500

    First commit

---
+++ start.py
@@ -0,0 +1,18 @@
+from time import sleep
+from flask import Flask
+from check_turtlebot import check_turtlebot
+import subprocess
+app = Flask(__name__)
+
+@app.route('/')
+def starting_up():
+    proc = subprocess.Popen(['roslaunch', 'turtlebot3_gazebo', 
+    'turtlebot3_stage_1.launch'], stdout=subprocess.DEVNULL, stderr= subprocess.DEVNULL)
+
+
+    if check_turtlebot():
+        return 'TurtleBot sucessfully run!'
+        
+    return 'TurtleBot run unsucessfull!'
+
+app.run(debug= True)
