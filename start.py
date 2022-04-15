9cc43b5593505f0e7bfb814f54871a712c402eb9
lastwalkerb <jawadmustafa571@gmail.com>
2022-04-15 16:43:11 +0500

    add gui

--- start.py
+++ start.py
@@ -6,17 +6,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def starting_up():
-
-    # proc_0 = subprocess.Popen(['export', 'TURTLEBOT3_MODEL=waffle'])
-
     proc = subprocess.Popen(['roslaunch', 'turtlebot3_gazebo', 
-    'turtlebot3_stage_1.launch']) 
-    
-    # stdout=subprocess.DEVNULL, stderr= subprocess.DEVNULL)
-    print(proc)
+    'turtlebot3_stage_1.launch'], stdout=subprocess.DEVNULL, stderr= subprocess.DEVNULL)
 
-    # if check_turtlebot():
-    #     return 'TurtleBot sucessfully run!'
+
+    if check_turtlebot():
+        return 'TurtleBot sucessfully run!'
+
+    
         
     return 'TurtleBot run unsucessfull!'
 
